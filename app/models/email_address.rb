43b48c132215feed3cd044975b9b74b2481cf567
Carmer <carmerandrew@gmail.com>
2015-04-18 16:37:22 -0600

    added validations for address and person_id. wrote tests to varify validity.

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,4 +1,7 @@
 class EmailAddress < ActiveRecord::Base
   belongs_to :person
 
+  validates :address, presence: true
+  validates :person_id, presence: true
+
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) do
+    EmailAddress.new(address: 'carmerandrew@gmail.com', person_id: 1)
+  end
+
+  it "is valid" do
+    expect(email_address).to be_valid
+  end
+
+  it "is invalid without an email address" do
+    email_address = EmailAddress.new(address: nil, person_id: 1)
+    expect(email_address).not_to be_valid
+  end
+
+  it ' is invalid without a person_id' do
+    email_address = EmailAddress.new(address: 'carmerandrew@gmail.com', person_id: nil)
+    expect(email_address).not_to be_valid
+  end
+
 end
